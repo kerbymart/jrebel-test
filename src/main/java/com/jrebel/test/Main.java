3ffaba789b808f051b39e2bd173c53b1b02af1cc
kerbymart <kerbymart@gmail.com>
2014-02-21 01:13:15 +0800

    Add JRebel plugin into the POM

--- src/main/java/com/jrebel/test/Main.java
+++ src/main/java/com/jrebel/test/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] vargs) throws Exception {
 		int counter = 0;
 		while(true){
-			System.out.println(counter + " - jRebel rocks! :D");
+			System.out.println(counter + " - jRebel rocks!!! :D"); // then hit CRTL + F9
 			Thread.sleep(1000);
 			counter++;
 		}
